67659efc811dffb7da938cef0b7305fdf42304e2
Gage Lodba <JerimiahOfficial@gmail.com>
2023-05-25 15:53:37 -0400

    Cleaned up cmake files, still working on main.cpp.

--- scripts/imgui.cmake
+++ scripts/imgui.cmake
@@ -6,24 +6,4 @@ FetchContent_Declare(
     GIT_TAG        master
 )
 
-FetchContent_GetProperties(imgui)
-if(NOT imgui_POPULATED)
-    FetchContent_Populate(imgui)
-
-    file(GLOB SRC_IMGUI
-        "${imgui_SOURCE_DIR}/*.cpp"
-        "${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp"
-        "${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp"
-    )
-
-    add_library(imgui STATIC ${SRC_IMGUI} )
-    source_group(TREE ${imgui_SOURCE_DIR} PREFIX "imgui" FILES ${SRC_IMGUI})
-
-    target_include_directories(imgui PRIVATE
-        "${imgui_SOURCE_DIR}"
-        "${imgui_SOURCE_DIR}/backends"
-        "${imgui_SOURCE_DIR}/misc/cpp"
-    )
-endif()
-
-set_property(TARGET imgui PROPERTY CXX_STANDARD 20)
+FetchContent_MakeAvailable(imgui)
